74b945a3e6b6ff95949d8992814d1242d0b7861e
Bikoil <bikokhaled13@gmail.com>
2024-10-14 01:12:29 +0300

    Formatted how the code is arranged

--- src/ls/ls.go
+++ src/ls/ls.go
@@ -1,30 +1,30 @@
 package main
 
 import (
-    "fmt"
-    "os"
-    "log"
+  "fmt"
+  "os"
+  "log"
 )
 
 func main() {
-    if len(os.Args) > 1 {
-        dir := os.Args[1]
-        files, err := os.ReadDir(dir)
-        if err != nil { 
-            log.Fatal(err)
-        }
-
-        for _, file := range files { 
-            fmt.Println(file.Name())
-        }
-    } else {
-        files, err := os.ReadDir(".")
-        if err != nil { 
-            log.Fatal(err)
-        }
-
-        for _, file := range files { 
-            fmt.Println(file.Name())
-        }
+  if len(os.Args) > 1 {
+    dir := os.Args[1]
+    files, err := os.ReadDir(dir)
+    if err != nil { 
+      log.Fatal(err)
     }
+
+    for _, file := range files { 
+      fmt.Println(file.Name())
+    }
+  } else {
+      files, err := os.ReadDir(".")
+      if err != nil { 
+        log.Fatal(err)
+      }
+
+      for _, file := range files { 
+        fmt.Println(file.Name())
+      }
+   }
 }
